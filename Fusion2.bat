27300e92eafec5e4e7a5e9c339d2078bec31fa73
Deivid Duarte <48442376+deividduarte20@users.noreply.github.com>
2021-02-02 16:25:37 -0300

    Alterado algumas informações.

--- Fusion2.bat
+++ Fusion2.bat
@@ -1,7 +1,7 @@
 ﻿
 ;VARIÁVEIS
 
-set ORIGEM=\\192.168.2.2\FUSION-PACOTE
+set ORIGEM=\\IP_SERVIDOR\FUSION-PACOTE
 set DESTINO=c:\temp\install\fusion
 
 ;INSTALAÇÃO FUSION X86
@@ -11,7 +11,7 @@ IF NOT EXIST "%PROGRAMFILES(X86)%" (
 
 if not exist %ProgramFiles%\FusionInventory-Agent (
 	
-	start %ORIGEM%\fusioninventory-agent_windows-x86_2.6.exe /S /acceptlicense /no-start-menu /server='http://192.168.2.189/glpi/plugins/fusioninventory/' /runnow /add-firewall-exception /installtasks=Full /execmode=Service
+	start %ORIGEM%\fusioninventory-agent_windows-x86_2.6.exe /S /acceptlicense /no-start-menu /server='http://IP_SERVIDOR_GLPI/glpi/plugins/fusioninventory/' /runnow /add-firewall-exception /installtasks=Full /execmode=Service
 	
 )
  )
@@ -23,6 +23,6 @@ IF EXIST "%PROGRAMFILES(X86)%" (
 
 if not exist %ProgramFiles%\FusionInventory-Agent (
 	
-	start %ORIGEM%\fusioninventory-agent_windows-x64_2.6.exe /S /acceptlicense /no-start-menu /server='http://192.168.2.189/glpi/plugins/fusioninventory/' /runnow /add-firewall-exception /installtasks=Full /execmode=Service
+	start %ORIGEM%\fusioninventory-agent_windows-x64_2.6.exe /S /acceptlicense /no-start-menu /server='http://IP_SERVIDOR_GLPI/glpi/plugins/fusioninventory/' /runnow /add-firewall-exception /installtasks=Full /execmode=Service
 )
  )
