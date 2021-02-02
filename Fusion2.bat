9ec74bbb1593192389981dfe8558a9a16f79fedd
Deivid Duarte <48442376+deividduarte20@users.noreply.github.com>
2021-02-02 14:16:55 -0300

    Fusion2.bat adicionando arquivo de instalação de agente fusion inventory via GPO

---
+++ Fusion2.bat
@@ -0,0 +1,28 @@
+﻿
+;VARIÁVEIS
+
+set ORIGEM=\\192.168.2.2\FUSION-PACOTE
+set DESTINO=c:\temp\install\fusion
+
+;INSTALAÇÃO FUSION X86
+
+IF NOT EXIST "%PROGRAMFILES(X86)%" ( 
+
+
+if not exist %ProgramFiles%\FusionInventory-Agent (
+	
+	start %ORIGEM%\fusioninventory-agent_windows-x86_2.6.exe /S /acceptlicense /no-start-menu /server='http://192.168.2.189/glpi/plugins/fusioninventory/' /runnow /add-firewall-exception /installtasks=Full /execmode=Service
+	
+)
+ )
+
+;INSTALAÇÃO FUSION X64
+
+IF EXIST "%PROGRAMFILES(X86)%" ( 
+
+
+if not exist %ProgramFiles%\FusionInventory-Agent (
+	
+	start %ORIGEM%\fusioninventory-agent_windows-x64_2.6.exe /S /acceptlicense /no-start-menu /server='http://192.168.2.189/glpi/plugins/fusioninventory/' /runnow /add-firewall-exception /installtasks=Full /execmode=Service
+)
+ )
