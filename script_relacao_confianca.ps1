cf6b3993bc5902b2049502fff020e8cca7db906e
Deivid Duarte <48442376+deividduarte20@users.noreply.github.com>
2022-05-20 19:41:12 -0300

    Script powershell refaz a relação de confiança
    Não precisa reiniciar

---
+++ script_relacao_confianca.ps1
@@ -0,0 +1,5 @@
+﻿# Caso nunca tenha executado script powershell execute esse primeiro comando
+# Set-ExecutionPolicy Unrestricted
+
+# Esse comando refaz a relação de confiança sem precisar reiniciar
+Test-ComputerSecureChannel -Repair -Credential *
